2b8169172a5cb8878aed4f2148c514ac05dfb27b
szt2mosjos <115459653+mosenlechnerjosef@users.noreply.github.com>
2023-12-04 12:03:59 +0100

    Add files via upload
    asd

---
+++ Koridok/Koridok/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Koridok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new();
+            #region 2. feladat
+            using (var sr = new StreamReader("autoverseny.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var sorTomb = sr.ReadLine().Split(";");
+
+                    versenyzok.Add(new Versenyzo(sorTomb[0], sorTomb[1], Convert.ToInt32(sorTomb[2]), sorTomb[3],TimeSpan.Parse(sorTomb[4]), Convert.ToInt32(sorTomb[5])));
+                }
+            }
+            #endregion
+            #region 3. feladat
+            Console.WriteLine("3. feladat: " + versenyzok.Count);
+            #endregion
+            #region 4. feladat
+            var versenyzo_szurt = versenyzok.Where(x => x.Nev == "Fürge Ferenc" && x.Palya == "Gran Prix Circuit" && x.Kör == 3).ToList();
+            versenyzo_szurt.ForEach(x => Console.WriteLine("4. feladat: " + (x.Korido.Seconds + x.Korido.Minutes * 60) + " másodperc"));
+            #endregion
+            #region 5.feladat
+            Console.WriteLine("5. feladat:");
+            Console.WriteLine("Kérem egy versenyző nevét:");
+            #endregion
+            #region 6. feladat
+            Console.Write("6. feladat: ");
+            var versenyzo_nev = Console.ReadLine();
+            var leggyorsabbKor = versenyzok.Where(x => x.Nev == versenyzo_nev).OrderBy(x => x.Korido).FirstOrDefault();
+            if (leggyorsabbKor != null)
+            {
+                Console.Write(leggyorsabbKor.Palya + ", " + leggyorsabbKor.Korido);
+            }
+            else
+            {
+                Console.Write("Nincs ilyen versenyző az állományban");
+            }
+            #endregion
+
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Koridok/Koridok/Versenyzo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koridok
+{
+    public class Versenyzo
+    {
+        public Versenyzo(string csapat, string nev,int kor, string palya, TimeSpan korido, int kör)
+        {
+            Csapat = csapat;
+            Nev = nev;
+            Kor = kor;
+            Palya = palya;
+            Korido = korido;
+            Kör = kör;
+            
+        }
+        public string Csapat { get; set; }  
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+        public string Palya { get; set; }
+        public TimeSpan Korido { get; set;}
+        public int Kör { get; set;}
+
+        
+    }
+}
